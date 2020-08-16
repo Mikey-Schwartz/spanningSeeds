883d07aeca5a2a64d4fdbae3ef0d0e582a7bbc6f
Avraham Kahan <avrahamkahan123@gmail.com>
2020-08-17 00:02:21 +0300

    Update spanningSeeds2.h

--- spanningSeeds2.h
+++ spanningSeeds2.h
@@ -2,4 +2,5 @@
 #define SPANNINGSEED2S_H
 int * generateEvenOdd(int upperBound);
 int * findRangeSeeds(int lowerBound, int upperBound, const int *precomputedEvenOdd);
+int findNumSeeds(int tap, const int *precomputedEvenOdd);
 #endif
