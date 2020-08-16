3068fbc96be5ff846f1e4adbd7f35daadcd0b46c
Avraham Kahan <avrahamkahan123@gmail.com>
2020-08-17 00:01:50 +0300

    Update threading.h

--- threading.h
+++ threading.h
@@ -1,8 +1,9 @@
 #ifndef THREADING_H
 #define THREADING_H
 
-int* parralelizeAnswer(int end, int *lookupTable);
+int* parralelizeAnswer(int start, int end, int *lookupTable);
 int* splitUpJob(int upperBound, int numProcessors);
 int getTotalWork(int upperBound);
+int * splitUpJob(int lowerBound, int upperBound, int numProccesors);
 
 #endif
