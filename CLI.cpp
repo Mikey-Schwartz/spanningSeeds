215480189159955f7f757ef91197d58e25d1b7cc
Avraham Kahan <avrahamkahan123@gmail.com>
2020-08-16 23:58:35 +0300

    Update CLI.cpp

--- CLI.cpp
+++ CLI.cpp
@@ -1,57 +1,29 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include <string.h>
 #include <stdlib.h>
 #include <sstream>
 #include <fstream>
 #include <time.h>
 #include "spanningSeeds2.h"
 #include "threading.h"
+#include "errorMessages.h"
 
 
+using std::cout; using std::endl; using std::unordered_map; using std::to_string;
+using std::string; using std::stringstream; using std::ofstream;
 
-using std::vector; using std::unordered_map; using std::string; using std::stringstream; using std::ofstream;
+// function declarations
+void populateArguments(int argc, char *argv[], unordered_map<char, int> &arguments);
+void tokenizeInput(int argc, char *argv[], unordered_map<char, int> &arguments);
+void loadIntParameter(char field, char *number, unordered_map<char, int> &arguments);
+void checkForInvalidInputs(unordered_map<char, int> &arguments);
+void runProgram(unordered_map<char, int> &arguments);
+void outputSingleAnswer(unordered_map<char, int> &arguments, int answer);
+void outputRangeAnswers(unordered_map<char, int> &arguments, int *answer);
 
-void runProgram(unordered_map<char, int> &arguments) {
-	int *lookupTable = generateEvenOdd()
-	if (arguments.find("n") != arguments.end()) {
-		int answer = findNumSeeds(arguments[n], lookupTable);
-		outputAnswer(arguments, answer);
-	}
-
-	int *lookupTable = generateEvenOdd()
-	if (arguments["p"] == 1) {
-
-	}
-}
-
-void outputAnswer(unordered_map<char, int> &arguments, int answer) {
-	if (arguments["f"] == 1) {
-		ofstream answerFile;
-		time_t currentTime = time(NULL);
-		answerfile.open(ctime(&currentTime) + "answers.txt");
-		outfile << "Format SEED NUMBER" << endl;
-		outfile << arguments["n"] << " " << answer << endl;
-		outfile.close();
-	}
-	else {
-		cout << "Format SEED NUMBER" << endl;
-		cout << arguments["n"] << " " << answer << endl;
-	}
-}
-
-void outputAnswers(nordered_map<char, int> &arguments, int* answer) {
-	if (arguments["f"] == 1) {
-		ofstream answerFile;
-		time_t currentTime = time(NULL);
-		answerfile.open(ctime(&currentTime) + "answers.txt");
-		outfile << "Format SEED NUMBER" << endl;
-		outfile << arguments["n"] << " " << answer << endl;
-		outfile.close();
-	}
-}
-
-void populateArguments(int argc, char *argv[], unordered_map<char, int> arguments) {
+void populateArguments(int argc, char *argv[], unordered_map<char, int> &arguments) {
 	if (argc == 1) {
 		cout << getEmptyMessage() << endl;
 		exit(-1);
@@ -60,132 +32,147 @@ void populateArguments(int argc, char *argv[], unordered_map<char, int> argument
 		tokenizeInput(argc, argv, arguments);
 	}
 	checkForInvalidInputs(arguments);
-	runProgram(arguments);
 }
 
-void checkForInvalidInputs(unordered_map<char, int> &arguments) {
-	unordered_map<char, int>::const_iterator parameter1;
-	unordered_map<char, int>::const_iterator parameter2;
-	if (arguments.find("n") != arguments.end() && (arguments.find("h") != arguments.end()) {
-		invalidIntegerCombinationError(); // exit
-	}
-	else if (arguments.find("h") == arguments.end()) {
-		noUpperBoundError();
-	}
-	else if(arguments["l"] < 0) {
-		negativeLowerBoundError();
-	}
-	else if (arguments["l"] >= arguments["h"]) {
-		upperLowerError();
-	}
-}
 
-void negativeLowerBoundError() {
-	cout << "Negative lower bound provided" << endl;
-	offerHelp();
-	exit();
-}
-
-void noUpperBoundError() {
-	cout << "No upper bound or specific seed provided. One of the two must be provided" << endl;
-	offerHelp();
-	exit();
-}
-
-void upperLowerError() {
-	cout << "provided lower bound greater or equal to provided upper bound" << endl;
-	offerHelp();
-	exit()
-}
-
-void invalidIntegerCombinationError() {
-	cout << "Invalid combination of integer options. Please remember that if the -n field is set, -h cannot also be set" << endl;
-	offerHelp();
-	exit();
-}
-
-void offerHelp() {
-	cout << "type 'registerSequences --help' for help" << endl;
-}
-
-void tokenizeInput(unordered_map<char, int> &arguments) {
-	if (argv[2] == "--help") {
+void tokenizeInput(int argc, char *argv[], unordered_map<char, int> &arguments) {
+	char help[] = "--help";
+	if (strcmp(argv[1], help) == 0) {
 		helpMessage(); // exits
 	}
 	bool lastWasIntegerArg = false;
-	char lastParam, field;
+	char lastParam;
+	char field;
 	unordered_map<char, int>::const_iterator parameter;
-	for (int i = 1; i < argc, i++) {
-		if (argv[i][0] != "-" && !lastWasIntegerArg) {
+	for (int i = 1; i < argc; i++) {
+		field = argv[i][1];
+		if (argv[i][0] != '-' && !lastWasIntegerArg) {
 			syntaxError(); // invalid syntax
 		}
-		field = argv[i][1];
 		else if (lastWasIntegerArg) {
 			loadIntParameter(lastParam, argv[i], arguments);
 			lastWasIntegerArg = false;
 		}
-		else if (field == "h" || field == "n" ||field == "l") {  // may cause problems with char array
+		else if (field == 'h' || field == 'n' ||field == 'l') {  // may cause problems with char array
 			lastWasIntegerArg = true;
-			lastParam = argv[i];
+			lastParam = field;
 		}
 		else {
-			parameter = arguments.find(argv[i]);
+			parameter = arguments.find(field);
 			if (parameter == arguments.end()) {
-				invalidArgumentError();
+				invalidArgumentError(field);
 			}
 			else {
-				arguments[argv[i]] = 1;
+				arguments[field] = 1;
 			}
 		}
 	}
-}
-
-void invalidArgumentError() {
-	cout << "Invalid Argument: '" << argv[i] << "' " << endl;
-	offerHelp();
-	exit();
-}
-
-void syntaxError() {
-	cout << "Improper syntax" << endl;
-	offerHelp();
-	exit()
 }
 
 void loadIntParameter(char field, char *number, unordered_map<char, int> &arguments) {
 	stringstream convertToInt{number};
 	int integerValue = -1;
 	if (!(convertToInt >> integerValue)) {
-		cout << "Non integer value supplied to integer field" << endl;
-		offerHelp();
-		exit()
+		nonIntegerParam();
 	}
 	else {
-		arguments[field] = integerValue;
+		if (field == 'l') {
+			arguments['l'] = integerValue;
+		}
+		else {
+			arguments.insert({field, integerValue});
+		}
 	}
 }
 
-void helpMessage() {
-	cout << "Invalid Input" << endl;
-	cout << getEmptyMessage() << endl;
-	exit();
+
+
+void checkForInvalidInputs(unordered_map<char, int> &arguments) {
+	unordered_map<char, int>::const_iterator parameter1;
+	unordered_map<char, int>::const_iterator parameter2;
+	bool nExists = arguments.find('n') != arguments.end();
+	bool hExists = arguments.find('h') != arguments.end();
+	bool lExists = arguments['l'] != 0;
+	if (nExists && (hExists || lExists)) {
+		invalidIntegerCombinationError(); // exit
+	}
+	else if (!nExists && !hExists) {
+		noUpperBoundError();
+	}
+	else if(arguments['l'] < 0) {
+		negativeBoundError();
+	}
+	else if ((nExists && arguments['n'] < 0) || (hExists && arguments['h'] < 0)) {
+		negativeBoundError();
+	}
+	else if (hExists && arguments['l'] >= arguments['h']) {
+		upperLowerError();
+	}
 }
 
-string getEmptyMessage() {
-	string errorMessage = "No range of seeds or single seed specified! Program Terminated \n "
-			"Author Avraham Kahan and Mikey Schwartz (avrahamkahan123@gmail.com)\n"
-			"Usage: $ registerSequences {-n | -h} [number] 
-			"Optional Arguments "
-			"-p for parralel computation. Default = false\n"
-			"-f for file output. Default = false\n"
-			"-l for command line. Default = true\n"
-	return errorMesssage;
+
+
+void runProgram(unordered_map<char, int> &arguments) {
+	int *lookupTable;
+	if (arguments.find('n') != arguments.end()) {
+		int *lookupTable = generateEvenOdd(arguments['n']);
+		int answer = findNumSeeds(arguments['n'], lookupTable);
+		outputSingleAnswer(arguments, answer);
+	}
+	else if (arguments['p'] == 1 && (arguments['h'] - arguments['l'] > 50)) {
+		arguments['h']+=1;
+		lookupTable = generateEvenOdd(arguments['h']);
+		int *parralelAnswer = parralelizeAnswer(arguments['l'], arguments['h'], lookupTable);
+		arguments['h']-=1;
+		outputRangeAnswers(arguments, parralelAnswer);
+	}
+	else {
+		lookupTable = generateEvenOdd(arguments['h']);
+		int *rangeAnswer = findRangeSeeds(arguments['l'], arguments['h'], lookupTable);
+		outputRangeAnswers(arguments, rangeAnswer);
+	}
 }
+
+void outputSingleAnswer(unordered_map<char, int> &arguments, int answer) {
+	if (arguments['f'] == 1) {
+		ofstream answerFile;
+		answerFile.open(to_string(arguments['n']) + "_answer.txt");
+		answerFile << "SEED NUMBER" << endl;
+		answerFile << arguments['n'] << " " << answer << endl;
+		answerFile.close();
+	}
+	else {
+		cout << "SEED NUMBER" << endl;
+		cout << arguments['n'] << " " << answer << endl;
+	}
+}
+
+void outputRangeAnswers(unordered_map<char, int> &arguments, int *answer) {
+	if (arguments['f'] == 1) {
+		ofstream answerFile;
+		answerFile.open(to_string(arguments['l']) +  "-" + to_string(arguments['h']) + "answers.txt");
+		answerFile << "SEED NUMBER" << endl;
+		for (int i = 0; i <= arguments['h'] - arguments['l']; ++i) {
+			answerFile << arguments['l'] + i << " " << answer[i] << '\n';
+		}
+		answerFile.close();
+	}
+	else {
+		for (int i = 0; i <= arguments['h'] - arguments['l']; ++i) {
+			cout << arguments['l'] + i << " " << answer[i] << "\n" << endl;
+		}
+	}
+}
+
+
 int main(int argc, char *argv[]) {
+
 	unordered_map<char, int> arguments = {
-			{"p", 0}, // parralel
-			{"l", 1}, // lower bound. Default = 1
-			{"f", 0}, // writes to file
+			{'p', 0}, // parallel
+			{'l', 0}, // lower bound. Default = 1
+			{'f', 0}, // writes to newline separated file
 	};
 	populateArguments(argc, argv, arguments);
+	runProgram(arguments);
+
 }
