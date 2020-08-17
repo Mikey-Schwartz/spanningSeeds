628f2f00fdef6b51fff98df41af9037db7e02c9c
Avraham Kahan <avrahamkahan123@gmail.com>
2020-08-17 17:57:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 # run this script as superuser to install the tool. Will install as registerSequences and download source code. Can also be used as an installation guide
 cd ~/Downloads
 git clone https://github.com/AvrahamKahan123/spanningSeeds/
-cd spanningSeeds/newestVersion
+cd spanningSeeds/newVersion
 g++ -pthread CLI.cpp errorMessages.cpp spanningSeeds2.cpp threading.cpp -o registerSequences 
 sudo mv numberSequences /usr/local/bin/
